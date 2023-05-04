420b74e62c49b10e46b9ec972616a82e96c07722
MrGoodBytes <bytes@mrgoodbytes.dev>
2023-05-04 09:24:57 -0500

    Add new properties to Symfony object

--- Tests/SymfonyTestTrait.php
+++ Tests/SymfonyTestTrait.php
@@ -16,7 +16,9 @@ trait SymfonyTestTrait
         return $symfony->setLatestStableVersion('6.2')
             ->setSupportedVersions(['5.4', '6.2'])
             ->setMaintainedVersions(['5.4', '6.2', '6.3'])
-            ->setSymfonyVersions($this->getSymfonyVersions());
+            ->setSymfonyVersions($this->getSymfonyVersions())
+            ->setSecurityMaintainedVersions(['4.4'])
+            ->setFlexSupportedVersions(['3.4', '4.0', '4.1', '4.2', '4.3', '4.4', '5.0', '5.1', '5.2', '5.3', '5.4', '6.0', '6.1', '6.2', '6.3']);
     }
 
     /**
@@ -36,6 +38,8 @@ trait SymfonyTestTrait
         return $normalizedSymfony->setLatestStableVersion('6.2.0.0')
             ->setSupportedVersions(['5.4.0.0', '6.2.0.0'])
             ->setMaintainedVersions(['5.4.0.0', '6.2.0.0', '6.3.0.0'])
-            ->setSymfonyVersions($this->getSymfonyVersions());
+            ->setSymfonyVersions($this->getSymfonyVersions())
+            ->setSecurityMaintainedVersions(['4.4.0.0'])
+            ->setFlexSupportedVersions(['3.4.0.0', '4.0.0.0', '4.1.0.0', '4.2.0.0', '4.3.0.0', '4.4.0.0', '5.0.0.0', '5.1.0.0', '5.2.0.0', '5.3.0.0', '5.4.0.0', '6.0.0.0', '6.1.0.0', '6.2.0.0', '6.3.0.0']);
     }
 }
